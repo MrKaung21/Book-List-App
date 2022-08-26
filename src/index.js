9f8e1d5060309e5ae0d26cb590a5fc048ee202db
Kaung Myat Tun <kaungtun111@gmail.com>
2022-08-26 23:56:13 +0630

    adding css styles

--- src/index.js
+++ src/index.js
@@ -2,22 +2,37 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import './index.css'
 
-const title = 'Destinations of a Lifetime'
-const author = 'Amelia Hepworth'
-const img = 'https://m.media-amazon.com/images/I/91CU55zNNaL._AC_UL320_.jpg'
+const firstBook = {
+    img: 'https://m.media-amazon.com/images/I/91CU55zNNaL._AC_UL320_.jpg',
+    title: 'Destinations of a Lifetime',
+    author: 'Amelia Hepworth'
+}
+
+const secondBook = {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL600_SR600,400_.jpg',
+    title: 'Our Class is a Family',
+    author: 'Shannon Olsen'
+}
+
+const thirdBook = {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/61Ars9rdgGS._AC_UL600_SR600,400_.jpg',
+    title: 'A Letter From Your Teacher',
+    author: 'Shannon Olsen'
+}
 
 function BookList() {
     return (
         <section className='booklist'>
-            <Book />
-            <Book />
+            <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} />
+            <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
+            <Book img={thirdBook.img} title={thirdBook.title} author={thirdBook.author} />
         </section>
     )
 }
 
 
 const Book = (props) => {
-    
+    const { img, title, author } = props
     return (
         <article className='book'>
             <img src={img} alt='' />
